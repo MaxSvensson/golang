fb9d5163035438472149d0b5feb935007d5fd643
Maximilian Svensson <Max.svensson@outlook.com>
2021-02-26 07:01:58 +0100

    first commit

---
+++ cards/deck.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+// Create new type of deck
+// Wich is a slice of strings
+type deck []string
+
+func newDeck() deck { 
+	 cards := deck{};
+	 cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"};
+	 cardValues := []string{"Ace", "Two", "Three", "Four"};
+
+	 for _, suit := range cardSuits {
+		 for _, value := range cardValues {
+			 cards = append(cards, value+ " Of "+suit)
+		 }
+	 }
+
+	 return cards;
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")	
+}
+
+func (d deck) saveTofile(fileName string) error {
+	error := ioutil.WriteFile(fileName, []byte(d.toString()), 0666)
+	return error
+}
+
+func newDeckFromFile(fileName string) deck {
+	bs, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		// Option #1 - log the error and return a call to newDeck()
+		// Option #2 - Log the error and entirelyquit the program
+		fmt.Println("Error: ",err)
+		os.Exit(1) 
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+
+func (d deck) shuffle() {
+
+	source := rand.NewSource(time.Now().UnixNano());
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
+}
---
+++ helloworld/main.go
@@ -0,0 +1,9 @@
+// When Specifying main as the package, it means that it should generate an exe file
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi there!")
+}
+
